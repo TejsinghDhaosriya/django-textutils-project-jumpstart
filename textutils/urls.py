6f9cf3657f66b2780a853d2d8e4f14bd086c7e89
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-20 21:52:37 +0530

    urls

--- textutils/urls.py
+++ textutils/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index"),
-    path('about/',views.about),
-    path('help',views.help),
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze'),
+    path('about', views.about, name='about')
+
 ]
